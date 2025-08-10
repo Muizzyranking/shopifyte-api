61a6377528d99ebd4334fb4665b9c634ab069b59
Muizzyranking <oyebowaleabdulmuiz@gmail.com>
2025-08-10 06:46:45 +0100

    feat: register models

--- apps/users/admin.py
+++ apps/users/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from django.apps import apps
 
-# Register your models here.
+app = apps.get_app_config("users")
+
+# automatically register all models in the app
+for models in app.get_models():
+    try:
+        admin.site.register(models)
+    except Exception:
+        pass
