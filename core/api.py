9b47304969f29c9dea46576d98d9c6133f7dfd7b
Muizzyranking <oyebowaleabdulmuiz@gmail.com>
2025-08-10 17:54:47 +0100

    refactor: use exception handler class

--- core/api.py
+++ core/api.py
@@ -1,36 +1,22 @@
 from ninja_extra import NinjaExtraAPI
+
 from apps.users.api import auth_router
+from core.exception_handler import setup_exception_handlers
 
 
-class CustomNinjaAPI(NinjaExtraAPI):
-    def create_response(self, request, data, *, status=200):
-        """
-        Overrides the default response creation to handle validation errors
-        """
-        if status == 422 and isinstance(data, dict) and "detail" in data:
-            errors = {}
-            if isinstance(data["detail"], list):
-                for error in data["detail"]:
-                    if isinstance(error, dict) and "loc" in error:
-                        field = error["loc"][-1] if error["loc"] else "general"
-                        message = error.get("ctx", {}).get("error", error.get("msg", ""))
-                        message = message.replace("Value error, ", "")
-
-                        if field not in errors:
-                            errors[field] = []
-                        errors[field].append(message)
-
-            data = {"message": "Validation failed", "errors": errors}
-
-        return super().create_response(request, data, status=status)
+class CustomNinjaExtraAPI(NinjaExtraAPI):
+    pass
 
 
-api = CustomNinjaAPI(
+api = CustomNinjaExtraAPI(
     title="SHOPIFYTE API Reference",
     description="API Reference for the SHOPIFYTE API",
     # docs_url=None,
     openapi_url="/schema.json",
     urls_namespace="api",
 )
+
+
+api = setup_exception_handlers(api)
 
 api.add_router("/auth/", auth_router)
