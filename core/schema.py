b926c8006b916c57b8dd1380848179c9cfe8babb
Muizzyranking <oyebowaleabdulmuiz@gmail.com>
2025-08-10 17:54:02 +0100

    feat: shared schemas

--- core/schema.py
+++ core/schema.py
@@ -1,5 +1,5 @@
 from ninja import Schema
-from typing import Generic, List, TypeVar
+from typing import Dict, Generic, List, TypeVar
 
 T = TypeVar("T")
 
@@ -18,3 +18,24 @@ class ErrorResponseSchema(BaseSchema):
 
 class DataResponseSchema(BaseSchema, Generic[T]):
     data: List[T]
+
+
+class ValidationErrorResponseSchema(BaseSchema):
+    message: str = "Invalid data"
+    errors: Dict[str, List[str]]
+
+
+class UnauthorizedResponseSchema(BaseSchema):
+    message: str = "Authentication required"
+
+
+class ForbiddenResponseSchema(BaseSchema):
+    message: str = "Permission denied"
+
+
+class NotFoundResponseSchema(BaseSchema):
+    message: str = "Resource not found"
+
+
+class BadRequestResponseSchema(BaseSchema):
+    message: str = "Bad request"
