e529726b9957d1b494a8487339e5617bae4aa3eb
Muizzyranking <oyebowaleabdulmuiz@gmail.com>
2025-08-10 06:49:20 +0100

    feat: verify email service

--- apps/users/services.py
+++ apps/users/services.py
@@ -1,3 +1,4 @@
+from core.services.verification import TokenType, VerificationService
 from .models import CustomUser
 
 
@@ -13,3 +14,19 @@ def create_user(user_data):
     user.set_password(user_data.get("password"))
     user.save()
     return user
+
+
+def verify_email_token(token: str) -> str:
+    verification_service = VerificationService()
+    data = verification_service.verify_token(token, TokenType.CONFIRMATION)
+    if not data["valid"]:
+        raise ValueError("Invalid or expired token.")
+    user_data = data["user"]
+    user = CustomUser.objects.filter(id=user_data.id, email=user_data.email).first()
+    if not user:
+        raise ValueError("Invalid token or user not found.")
+    if user.email_verified:
+        raise ValueError("Email is already verified.")
+    user.email_verified = True
+    user.save()
+    return "Email verification successful. You can now log in."
