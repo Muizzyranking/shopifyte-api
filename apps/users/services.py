f332d22a05316519d1a4d13d565efe4d5e226562
Muizzyranking <oyebowaleabdulmuiz@gmail.com>
2025-08-14 11:09:52 +0100

    refactor: unpack dictionary to create user

--- apps/users/services.py
+++ apps/users/services.py
@@ -8,14 +8,9 @@ from .models import CustomUser
 
 def create_user(user_data):
     user_data = user_data.dict()
-    user = CustomUser.objects.create(
-        email=user_data.get("email"),
-        username=user_data.get("username", None),
-        first_name=user_data.get("first_name"),
-        last_name=user_data.get("last_name"),
-        email_verified=False,
-    )
-    user.set_password(user_data.get("password"))
+    password = user_data.pop("password")
+    user = CustomUser.objects.create(**user_data)
+    user.set_password(password)
     user.save()
     return user
 
