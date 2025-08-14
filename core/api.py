8d25a10e7360e05ad09f194c673d111000588266
Muizzyranking <oyebowaleabdulmuiz@gmail.com>
2025-08-14 13:29:19 +0100

    feat: profile router

--- core/api.py
+++ core/api.py
@@ -1,6 +1,6 @@
 from ninja_extra import NinjaExtraAPI
 
-from apps.users.api import auth_router
+from apps.users.api import auth_router, profile_router
 from core.exception_handler import setup_exception_handlers
 
 
@@ -20,3 +20,4 @@ api = CustomNinjaExtraAPI(
 api = setup_exception_handlers(api)
 
 api.add_router("/auth/", auth_router)
+api.add_router("/profile/", profile_router)
