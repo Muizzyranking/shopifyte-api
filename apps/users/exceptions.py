a80af87cef9668edad1f7713d8cb141a4efed661
Muizzyranking <oyebowaleabdulmuiz@gmail.com>
2025-08-20 18:55:35 +0100

    fix: default message for user not found exception

--- apps/users/exceptions.py
+++ apps/users/exceptions.py
@@ -1,4 +1,6 @@
 class UserNotFound(Exception):
     """Raised when user is not found"""
 
-    pass
+    def __init__(self, message="User not found."):
+        self.message = message
+        super().__init__(self.message)
