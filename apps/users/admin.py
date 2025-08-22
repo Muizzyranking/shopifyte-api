6f99c788c7688a43869756fe6774560b52707fdf
Muizzyranking <oyebowaleabdulmuiz@gmail.com>
2025-08-22 14:02:41 +0100

    refactor: register all models

--- apps/users/admin.py
+++ apps/users/admin.py
@@ -1,11 +1,3 @@
-from django.contrib import admin
-from django.apps import apps
+from core.admin import register_all_models
 
-app = apps.get_app_config("users")
-
-# automatically register all models in the app
-for models in app.get_models():
-    try:
-        admin.site.register(models)
-    except Exception:
-        pass
+register_all_models("images")
