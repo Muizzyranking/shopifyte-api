84e84cd25830558f411274ba8b288134dd208d69
muizzyranking <oyebowaleabdulmuiz@gmail.com>
2025-08-29 23:36:33 +0100

    feat: utility function to create verification url

--- apps/users/utils.py
+++ apps/users/utils.py
@@ -1,6 +1,7 @@
 import random
 import string
 from django.http import HttpRequest
+from django.urls import reverse
 
 from apps.users.exceptions import UserNotFound
 from apps.users.models import CustomUser
@@ -13,10 +14,16 @@ def generate_code(length: int = 6) -> str:
     return "".join(random.choices(characters, k=length))
 
 
+def verification_url(request: HttpRequest, token: str):
+    namespace = getattr(request.resolver_match, "namespace", None)
+    path = reverse(f"{namespace}:verify_email", kwargs={"token": token})
+    return request.build_absolute_uri(path)
+
+
 def send_confirmation_email(request: HttpRequest, user: CustomUser):
     verification_service = TokenService()
     token = verification_service.generate_token(user, TokenType.CONFIRMATION)
-    confirmation_url = verification_service.create_verification_url(request, token, "verify_email")
+    confirmation_url = verification_url(request, token)
     email_service = EmailService()
     return email_service.send_confirmation_email(user, confirmation_url)
 
