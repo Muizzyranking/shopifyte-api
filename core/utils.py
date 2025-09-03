276033e9decaba08337fd7bae3e48256043a70e4
muizzyranking <oyebowaleabdulmuiz@gmail.com>
2025-09-03 22:33:16 +0100

    feat: utility function to get seconds

--- core/utils.py
+++ core/utils.py
@@ -8,3 +8,7 @@ def response_with_data(msg, data):
 
 def error_message(msg):
     return response_message(f"Error: {str(msg)}")
+
+
+def get_seconds(hours: float = 0, minutes: float = 0, seconds: float = 0) -> int:
+    return int((hours * 3600) + (minutes * 60) + seconds)
