a7fa158ad06694d56f789a1731173815aa4996e5
muizzyranking <oyebowaleabdulmuiz@gmail.com>
2025-09-11 15:24:30 +0100

    feat: schema to dict utility function

--- core/utils.py
+++ core/utils.py
@@ -1,5 +1,7 @@
+from typing import Union
 from django.apps import apps
 from django.contrib import admin
+from ninja import Schema
 
 
 def register_all_models(app_name):
@@ -25,3 +27,14 @@ def error_message(msg):
 
 def get_seconds(hours: float = 0, minutes: float = 0, seconds: float = 0) -> int:
     return int((hours * 3600) + (minutes * 60) + seconds)
+
+
+def schema_to_dict(schema: Union[Schema, dict, None]) -> dict:
+    if not schema:
+        return {}
+    elif hasattr(schema, "dict"):
+        return schema.dict(exclude_unset=True)
+    elif isinstance(schema, dict):
+        return schema
+    else:
+        return {}
