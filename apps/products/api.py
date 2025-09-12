f3e35c12aa3159dfdcd23ec74b6560809ecb2720
Muizzyranking <oyebowaleabdulmuiz@gmail.com>
2025-09-12 18:44:01 +0100

    feat: get single product by slug

--- apps/products/api.py
+++ apps/products/api.py
@@ -2,7 +2,7 @@ from ninja import Query
 from apps.products.services import ProductService
 from core.router import CustomRouter
 
-from .schemas import ProductFilters, ProductListSchema
+from .schemas import ProductDetailSchema, ProductFilters, ProductListSchema
 
 products_router = CustomRouter()
 
@@ -14,3 +14,11 @@ def get_products(request, filters: Query[ProductFilters]):
     """
     result = ProductService.get_products(request, filters)
     return result
+
+
+@products_router.get("{slug}", response={200: ProductDetailSchema})
+def get_product_details(request, slug: str):
+    return 200, {
+        "message": "Product successfully retrieved",
+        "data": ProductService.get_product_by_slug(slug),
+    }
